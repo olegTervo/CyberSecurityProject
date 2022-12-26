60f2bf1751495011ee25cbb0d77b2955a34d042a
olegTervo <olegtervo@icloud.com>
2022-12-26 14:35:08 +0200

    Notebook app template

--- notebook/notebook/asgi.py
+++ notebook/notebook/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for FlawsApp project.
+ASGI config for notebook project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FlawsApp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'notebook.settings')
 
 application = get_asgi_application()
