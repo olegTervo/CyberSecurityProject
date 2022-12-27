1c40b4586283870818a33a714081c09aecf0bea1
olegTervo <olegtervo@icloud.com>
2022-12-27 13:18:45 +0200

    models and database, gitignore

--- notebook/notebook/urls.py
+++ notebook/notebook/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.shortcuts import redirect
 
 urlpatterns = [
+	path('', lambda request: redirect('notebookapp/')),
 	path('notebookapp/', include('notebookapp.urls')),
 	path('admin/', admin.site.urls),
 ]
--- notebook/notebookapp/views.py
+++ notebook/notebookapp/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from notebookapp.db import *
+
 def index(request):
 	return HttpResponse("Wellcome")
 
+def listView(request):
+	res = getlist2(request.user)
+
+	return HttpResponse(res)
