c0d0c35475c568c27268d1591740471582c30de9
olegTervo <37378626+olegTervo@users.noreply.github.com>
2022-12-28 20:21:12 +0200

    Update settings.py

--- notebook/notebook/settings.py
+++ notebook/notebook/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1l8k$41dvap((2_)a7&i+y0g7c=30q#5$pd%3+e_9bgn9tgv)i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', '.localhost']
 
 
 # Application definition
